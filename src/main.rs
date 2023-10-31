6273f6897a40f20b870edee8ac0290328547fb2d
CHRISO1503 <christopher_oakes@hotmail.com>
2023-10-31 20:33:12 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::render::WindowCanvas;
+
+fn render(canvas: &mut WindowCanvas, color: Color) {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.present();
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("WIIINDOW", 800, 600)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("could not initialize canvas");
+
+    let mut event_pump = sdl_context.event_pump()?;
+    'running: loop {
+        // Handle events
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running;
+                }
+                _ => {}
+            }
+        }
+        render(&mut canvas, Color::RGB(0, 10, 200));
+    }
+    Ok(())
+}
